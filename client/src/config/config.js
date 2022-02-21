ac36d2cf8731c412ab8cdc69a5e235503bbbf8aa
arstanbek <arstanbek353@gmail.com>
2022-02-21 18:53:03 +0600

    change url to heroku url (env, client/config)

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,5 +1,5 @@
 export const baseUrl = ""
-export const API_URL = "http://localhost"
+export const API_URL = "https://floating-journey-29986.herokuapp.com"
 export const roles = {
     admin: "admin",
     viewer: "viewer",
